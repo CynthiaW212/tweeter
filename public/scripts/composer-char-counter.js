7f6ba1315a1af737f390bb62b04fbed3db884de9
Yuze Wang <yuzewang621@gmail.com>
2023-05-23 09:10:37 -0600

    update

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,9 +6,8 @@ $(document).ready(function() {
   $input.on('input', function() {
     let inputText = $(this).val();
     let $counter = $('output[name="counter"]');
-    console.log(this);
     let remainChars = 140 - inputText.length;
-    console.log(remainChars);
+    
     $counter.text(remainChars);
     if (remainChars < 0) {
       $counter.addClass('exceeded');
